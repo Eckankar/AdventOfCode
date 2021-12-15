9beb3b2889740e69b63086703307121f983c709e
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2021-12-15 11:06:45 +0100

    2021 day 15

---
+++ 2021/15/solve2.hs
@@ -0,0 +1,29 @@
+import qualified Data.Array as A
+import qualified Data.Heap as H
+import qualified Data.Map as M
+
+expandMap m = m'
+    where m'' = map (concat . take 5 . iterate (map inc1)) m
+          m'  = concat $ take 5 $ iterate (map (map inc1)) m''
+          inc1 = (+1) . (`mod` 9)
+
+neighbors (x, y) = [ (x-1, y), (x+1, y), (x, y-1), (x, y+1) ]
+
+type MinHeap = H.HeapT (H.Prio H.FstMinPolicy (Int, (Int, Int))) (Int, Int)
+
+shortestPath :: M.Map (Int, Int) Int -> M.Map (Int, Int) Int -> MinHeap -> (Int, Int) -> Int
+shortestPath m dm h e
+    | p == e          = dm' M.! e
+    | p `M.member` dm = shortestPath m dm  h' e
+    | otherwise       = shortestPath m dm' h' e
+    where Just ((v, p), h'') = H.view h
+          ns  = filter (\n -> n `M.member` m && not (n `M.member` dm)) $ neighbors p
+          h'  = H.filter ((/= p) . snd) $ foldr (\n -> H.insert (v + m M.! n, n)) h'' ns
+          dm' = M.insert p v dm
+
+main :: IO ()
+main = do
+    input <- fmap (expandMap . map (map ((0+) . read . (:[]))) . lines) getContents
+    let (height, width) = (length input, length $ head input)
+    let inputMap = M.fromList $ A.assocs $ A.listArray ((0, 0), (height-1, width-1)) $ concat input
+    print $ shortestPath inputMap M.empty (H.singleton (0, (0, 0))) $ (height-1, width-1)
