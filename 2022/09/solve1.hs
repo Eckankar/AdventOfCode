d0cd272249a627d7d02ea24495990a1282017c76
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2022-12-09 07:26:13 +0100

    2022 day 09

---
+++ 2022/09/solve1.hs
@@ -0,0 +1,42 @@
+import qualified Data.Set as S
+
+type Point = (Int, Int)
+
+data Direction = U | D | L | R
+    deriving (Show, Eq, Read)
+
+add :: Point -> Point -> Point
+add (a, b) (c, d) = (a+c, b+d)
+
+dirVec :: Direction -> Point
+dirVec U = ( 0, -1)
+dirVec D = ( 0,  1)
+dirVec L = (-1,  0)
+dirVec R = ( 1,  0)
+
+dist :: Point -> Point -> Int
+dist (a, b) (c, d) = max (abs (a-c)) (abs (b-d))
+
+follow :: Point -> Point -> Point
+follow p1 p2 | dist p1 p2 < 2 = p1
+follow (a, b) (c, d) = (diff a c, diff b d)
+    where diff x y | x /= y = x + signum (y-x)
+                   | x == y = x
+
+simulate :: Point -> Point -> [Direction] -> (Point, Point, S.Set Point)
+simulate head tail = foldl sim' init
+    where init = (head, tail, S.singleton tail)
+          sim' (head, tail, trail) dir = (head', tail', trail')
+            where head' = add head $ dirVec dir
+                  tail' = follow tail head'
+                  trail' = S.insert tail' trail
+
+parseLine :: String -> [Direction]
+parseLine (dir : ' ' : n) = replicate (read n) $ read [dir]
+
+main :: IO ()
+main = do
+    input <- fmap (concat . map parseLine . lines) getContents
+
+    let (head, tail, positions) = simulate (0, 0) (0, 0) input
+    print $ S.size positions
---
+++ 2022/09/solve2.hs
@@ -0,0 +1,44 @@
+import qualified Data.Set as S
+
+type Point = (Int, Int)
+
+data Direction = U | D | L | R
+    deriving (Show, Eq, Read)
+
+add :: Point -> Point -> Point
+add (a, b) (c, d) = (a+c, b+d)
+
+dirVec :: Direction -> Point
+dirVec U = ( 0, -1)
+dirVec D = ( 0,  1)
+dirVec L = (-1,  0)
+dirVec R = ( 1,  0)
+
+dist :: Point -> Point -> Int
+dist (a, b) (c, d) = max (abs (a-c)) (abs (b-d))
+
+follow :: Point -> Point -> Point
+follow p1 p2 | dist p1 p2 < 2 = p1
+follow (a, b) (c, d) = (diff a c, diff b d)
+    where diff x y | x /= y = x + signum (y-x)
+                   | x == y = x
+
+simulate :: [Point] -> [Direction] -> ([Point], S.Set Point)
+simulate rope = foldl sim' init
+    where init = (rope, S.singleton $ last rope)
+          sim' (rope, trail) dir = (r':rs', trail')
+            where (r:rs) = rope
+                  r' = add r $ dirVec dir
+                  (_:rs') = scanl (flip follow) r' rs
+                  trail' = S.insert (last rs') trail
+
+parseLine :: String -> [Direction]
+parseLine (dir : ' ' : n) = replicate (read n) $ read [dir]
+
+main :: IO ()
+main = do
+    input <- fmap (concat . map parseLine . lines) getContents
+
+    let initialRope = replicate 10 (0, 0)
+    let (finalRope, tailPositions) = simulate initialRope input
+    print $ S.size tailPositions
