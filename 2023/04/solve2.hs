6480f3949c674217fc34070749a978b8f5e6a752
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2023-12-04 06:28:57 +0100

    2023 day 4

---
+++ 2023/04/solve2.hs
@@ -0,0 +1,36 @@
+import qualified Data.Set as S
+
+import Control.Applicative ((<$>), Applicative(..), Alternative((<|>)))
+import Data.Char (isDigit)
+import Text.ParserCombinators.ReadP
+
+data Card = Card Int [Int] [Int]
+    deriving (Eq, Show)
+
+parseNumber :: ReadP Int
+parseNumber = read <$> munch1 isDigit
+
+parseInputLine = do
+    string "Card"
+    many1 $ char ' '
+    id <- parseNumber
+    string ":"
+    many1 $ char ' '
+    winning <- sepBy parseNumber (many1 $ char ' ')
+    string " |"
+    many1 $ char ' '
+    actual <- sepBy parseNumber (many1 $ char ' ')
+    eof
+    return $ Card id winning actual
+
+runParser p = fst . head . filter (\(_,"") -> True) . readP_to_S p
+
+scoreCard :: Card -> [Int] -> [Int]
+scoreCard (Card id winning actual) previous = cardScore : previous
+    where overlap = S.intersection (S.fromList winning) $ S.fromList actual
+          cardScore = 1 + (sum $ take (S.size overlap) previous)
+
+main :: IO ()
+main = do
+    input <- fmap (map (runParser parseInputLine) . lines) getContents
+    print $ sum $ foldr scoreCard [] input
